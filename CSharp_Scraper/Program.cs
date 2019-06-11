f4a6041c94f2b5f7574600511bca2379b3204123
milomacphail <milomacphail@gmail.com>
2019-06-11 18:56:46 -0400

    Created initial project schema

--- CSharp_Scraper/Program.cs
+++ CSharp_Scraper/Program.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 // Requires reference to WebDriver.Support.dll
 using OpenQA.Selenium.Support.UI;
 
@@ -15,38 +14,41 @@ class GoogleSuggest
 {
     static void Main(string[] args)
     {
-   
+
         IWebDriver driver = new ChromeDriver();
-        
-            //Notice navigation is slightly different than the Java version
-            //This is because 'get' is a keyword in C#
-            driver.Navigate().GoToUrl("https://finance.yahoo.com/");
 
-            // Find the text input element by its name
-            driver.FindElement(By.Id("uh-signedin")).Click();
+        //Notice navigation is slightly different than the Java version
+        //This is because 'get' is a keyword in C#
+        driver.Navigate().GoToUrl("https://finance.yahoo.com/");
 
-            //page load
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+        // Find the text input element by its name
+        driver.FindElement(By.Id("uh-signedin")).Click();
 
-            driver.FindElement(By.Id("login-username")).SendKeys("milomacphail@gmail.com");
-            driver.FindElement(By.Id("login-signin")).SendKeys(Keys.Enter);
+        //page load
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
 
-            //Wait until the page loads
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+        driver.FindElement(By.Id("login-username")).SendKeys("milomacphail@gmail.com");
+        driver.FindElement(By.Id("login-signin")).SendKeys(Keys.Enter);
 
-            driver.FindElement(By.Id("login-passwd")).SendKeys("Pandahead1");
-            driver.FindElement(By.Id("login-signin")).SendKeys(Keys.Enter);
+        //Wait until the page loads
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
 
-            driver.FindElement(By.LinkText("My Watchlist")).SendKeys(Keys.Enter);
+        driver.FindElement(By.Id("login-passwd")).SendKeys("Pandahead1");
+        driver.FindElement(By.Id("login-signin")).SendKeys(Keys.Enter);
 
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+        driver.FindElement(By.LinkText("My Watchlist")).SendKeys(Keys.Enter);
 
-            driver.FindElement(By.LinkText("DISC")).SendKeys(Keys.Enter);
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
 
-            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(5);
+        IWebElement table = driver.FindElement(By.XPath("//*[@id=\"pf-detail-table\"]/div[1]/table"));
 
+        IList<IWebElement> table_rows = driver.FindElements(By.TagName("tr"));
 
-
-
+        foreach (IWebElement table_row in table_rows)
+        {
+            Console.WriteLine(table_row);
         }
+
+        driver.Close();
     }
+}
---
+++ CSharp_Scraper/StockSchema.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace CSharp_Scraper
+{
+    public class stock
+    {
+        public string Symbol { get; set; }
+        public string LastPrice { get; set; }
+        public string Change { get; set; }
+        public string ChangePercent { get; set; }
+        public string Volume { get; set; }
+        public string Shares { get; set; }
+        public string AvgVol { get; set; }
+        public string MarketCap { get; set; }
+
+    }
+}
+
